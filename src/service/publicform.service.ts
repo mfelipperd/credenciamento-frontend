c656c79303c4c07fb1c63318a8f9f87237a396f9
mfelipperd <felipperabelodurans@gmail.com>
2025-05-06 00:27:24 -0300

    fix: imports

--- src/service/publicform.service.ts
+++ src/service/publicform.service.ts
@@ -1,12 +1,12 @@
 import { useAxios } from "@/hooks/useAxio";
-import type { CreateVisitor } from "@/pages/PublicForm/FormCreateVisitor";
+import type { CredenciamentoFormData } from "@/pages/PublicForm/FormCreateVisitor/schema";
 import { useState } from "react";
 
 export const usePublicFormService = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const api = useAxios();
 
-  const create = async (data: CreateVisitor) => {
+  const create = async (data: CredenciamentoFormData) => {
     try {
       setLoading(true);
       const response = await api.post("/visitors", data);
