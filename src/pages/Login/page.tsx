741460d717631497cd79f07fa30265ebde27725f
mfelipperd <felipperabelodurans@gmail.com>
2025-05-14 20:59:20 -0300

    feat: adjust mobile login

--- src/pages/Login/page.tsx
+++ src/pages/Login/page.tsx
@@ -2,10 +2,10 @@ import { LoginForm } from "./components/LoginForm";
 
 export const Login = () => {
   return (
-    <section className="flex flex-col items-center justify-center bg-neutral-100  lg:bg-white lg:py-40 scrollable-content">
+    <section className="flex flex-col items-center justify-center bg-neutral-100 h-[100vh] lg:bg-white lg:py-40 scrollable-content">
       <div className="w-full lg:w-fit p-5 flex flex-col gap-6 items-center   bg-neutral-100  lg:rounded-3xl sm:border-amber-300 lg:shadow-md ">
-        <img src="/logo.png" className="w-3xs" />
-        <h1 className="text-shadow-blue-800 uppercase text-center text-2xl font-bold w-96">
+        <img src="/logo.png" className="w-3xs animate-pulse" />
+        <h1 className="text-blue-950 uppercase text-center text-2xl font-bold w-96 animate-pulse">
           Login{" "}
         </h1>
         <LoginForm />
