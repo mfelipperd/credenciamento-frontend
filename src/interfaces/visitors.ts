d80ab0771b439602a8325b07a11e2183ea59fe8b
mfelipperd <felipperabelodurans@gmail.com>
2025-06-24 15:14:56 -0300

    feat: add graphic hours

--- src/interfaces/visitors.ts
+++ src/interfaces/visitors.ts
@@ -19,3 +19,11 @@ export interface AbsentVisitor {
   company: string;
   email: string;
 }
+
+export interface CheckinPerHourResponse {
+  hours: string[]; // Ex: ["08:00", "09:00", ..., "18:00"]
+  data: {
+    name: string; // Ex: "24/06/2025"
+    data: number[]; // Ex: [0, 1, 3, 0, ...] — 1 valor para cada hora
+  }[];
+}
--- src/pages/Dashboard/page.tsx
+++ src/pages/Dashboard/page.tsx
@@ -4,6 +4,7 @@ import { useDashboardController } from "./dashboard.controller";
 import { CategoryRadialChart } from "./components/CategoryRadialChart";
 import { OriginRadialChart } from "./components/OriginRadialChart";
 import { SectorsRadialChart } from "./components/SectorRadialChart";
+import { CheckinPerHourChart } from "./components/CheckinPerHourChat";
 
 export const Dashboard = () => {
   const controller = useDashboardController();
@@ -32,21 +33,23 @@ export const Dashboard = () => {
       </CardRoot>
       <CardRoot
         title="como conheceu"
-        className="bg-white sm:col-span-6 h-96 min-h-fit col-span-full"
+        className="bg-white sm:col-span-6  min-h-fit col-span-full"
       >
         <OriginRadialChart fairId={controller.fairId} />
       </CardRoot>{" "}
       <CardRoot
         title="Equipe do evento "
-        className="bg-white sm:col-span-4 h-96 col-span-full"
+        className="bg-white sm:col-span-4  col-span-full"
       />{" "}
       <CardRoot
-        title="mapa de inscritos "
-        className="bg-white sm:col-span-4 h-96 col-span-full"
-      />{" "}
+        title="Horarios de Checkins"
+        className="bg-white sm:col-span-4 col-span-full min-h-fit"
+      >
+        <CheckinPerHourChart fairId={controller.fairId} />
+      </CardRoot>
       <CardRoot
         title="Setor de interesse"
-        className="bg-white sm:col-span-4 h-96 col-span-full"
+        className="bg-white sm:col-span-4 col-span-full"
       >
         <SectorsRadialChart fairId={controller.fairId} />
       </CardRoot>
