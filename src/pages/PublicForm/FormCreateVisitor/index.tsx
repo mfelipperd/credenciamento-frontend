1e0c6a9f9099a3fcce80d49b5f74de55616c9e50
mfelipperd <felipperabelodurans@gmail.com>
2025-06-30 19:24:23 -0300

    feat: adjust form

--- src/pages/PublicForm/FormCreateVisitor/index.tsx
+++ src/pages/PublicForm/FormCreateVisitor/index.tsx
@@ -88,8 +88,8 @@ export const FormularioCredenciamento: React.FC = () => {
   const ingresso = watch("ingresso");
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="h-[80%] ">
-      <div className="h-full">
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
         <Controller
           control={control}
           name="ingresso"
--- src/pages/PublicForm/page.tsx
+++ src/pages/PublicForm/page.tsx
@@ -3,7 +3,7 @@ import { FormularioCredenciamento } from "./FormCreateVisitor";
 
 export const PublicForm = () => {
   return (
-    <section className="flex flex-col items-center justify-center bg-neutral-100  lg:bg-white lg:py-20 scrollable-content h-screen">
+    <section className="flex flex-col items-center justify-center bg-neutral-100  lg:bg-white lg:py-20 scrollable-content ">
       <div className="w-full lg:w-fit p-5   bg-neutral-100  lg:rounded-3xl sm:border-amber-300 lg:shadow-md h-full ">
         <h1 className="text-pink-600 uppercase text-center text-2xl font-semibold ">
           Faça seu credenciamento
